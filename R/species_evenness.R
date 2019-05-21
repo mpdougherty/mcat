a1fbec870a8529bc01a1197a5ffb8ee53ef85c2d
Michael P. Dougherty <Michael.P.Dougherty@usace.army.mil>
2019-05-21 16:54:52 -0500

    Added species_evenness function

---
+++ R/species_evenness.R
@@ -0,0 +1,58 @@
+#' @title Species Evenness
+#'
+#' @description Calculates the species evenness MCAT metric for the input
+#' individual mussel data frame.
+#'
+#' @export
+#' @param individuals    data frame; a data frame of individual mussel records.
+#'
+#' @return A data frame of sampled sites with the calculated species evenness
+#'         MCAT metric
+#'
+#' @examples
+#' # Create the individual mussel data frame
+#' individuals <- mcat::individuals
+#'
+#' # Calculate species evenness for the individuals data frame
+#' se <- species_evenness(individuals)
+#'
+#' @importFrom dplyr group_by summarize
+#' @importFrom labdsv matrify
+#' @importFrom vegan diversity specnumber
+#'
+species_evenness <- function(individuals) {
+  # Create the `number_live` field
+  individuals$number_live <- ifelse(individuals$Status == "Live", 1, 0)
+
+  # Group by SampleID
+  individuals %>%
+    dplyr::group_by(SampleID, Ename) %>%
+    dplyr::summarize(SUM_number_live = sum(number_live)) -> sample_species
+
+  # Convert sample_species from a grouped df to a regular data frame
+  sample_species_df <- as.data.frame(sample_species)
+
+  # Convert to vegan community data matrix-like format using labdsv::matrify
+  sample_species_matrix <- labdsv::matrify(sample_species_df)
+
+  # Calculate the Shannon-Weaver diversity index
+  shannon_diversity <- vegan::diversity(sample_species_matrix)
+
+  # Calculate the number species per site (see ?vegan::diversity)
+  species_number <- vegan::specnumber(sample_species_matrix)
+
+  # Use the following equation to calculate Pielou's evenness (See the vegan
+  # 'Diversity Vignette' for details).
+  pielou_evenness <- shannon_diversity/log(species_number)
+
+  # Set NaN pielou_evenness values to zero
+  pielou_evenness <- ifelse(is.nan(pielou_evenness), 0, pielou_evenness)
+
+  # Create a data frame of results
+  pielou <- data.frame(SampleID = names(pielou_evenness),
+                       shannon_diversity,
+                       species_number,
+                       pielou_evenness)
+
+  return(pielou)
+}
---
+++ tests/testthat/test_species_evenness.R
@@ -0,0 +1,47 @@
+context("species_evenness")
+
+# Create the individual mussel data frame
+individuals <- mcat::individuals
+
+# Calculate abundance for the individuals data frame
+se <- species_evenness(individuals)
+
+test_that("Check pielou_evenness for site 201009211401 = 0", {
+  se_201009211401 <- se[se$SampleID == "201009211401", ]
+  expect_equal(se_201009211401$pielou_evenness, 0, tolerance = 1e-2)
+})
+
+test_that("Check pielou_evenness for site 201009211404 = 0.9463946", {
+  se_201009211404 <- se[se$SampleID == "201009211404", ]
+  expect_equal(se_201009211404$pielou_evenness, 0.9463946, tolerance = 1e-2)
+})
+
+test_that("Check pielou_evenness for site 201009211408 = 0", {
+  se_201009211408 <- se[se$SampleID == "201009211408", ]
+  expect_equal(se_201009211408$pielou_evenness, 0, tolerance = 1e-2)
+})
+
+test_that("Check pielou_evenness for site 201009221409 = 1", {
+  se_201009221409 <- se[se$SampleID == "201009221409", ]
+  expect_equal(se_201009221409$pielou_evenness, 1, tolerance = 1e-2)
+})
+
+test_that("Check species_number for site 201009211401 = 1", {
+  se_201009211401 <- se[se$SampleID == "201009211401", ]
+  expect_equal(se_201009211401$species_number, 1, tolerance = 1e-2)
+})
+
+test_that("Check species_number for site 201009211404 = 3", {
+  se_201009211404 <- se[se$SampleID == "201009211404", ]
+  expect_equal(se_201009211404$species_number, 3, tolerance = 1e-2)
+})
+
+test_that("Check species_number for site 201009211408 = 0", {
+  se_201009211408 <- se[se$SampleID == "201009211408", ]
+  expect_equal(se_201009211408$species_number, 0, tolerance = 1e-2)
+})
+
+test_that("Check species_number for site 201009221409 = 2", {
+  se_201009221409 <- se[se$SampleID == "201009221409", ]
+  expect_equal(se_201009221409$species_number, 2, tolerance = 1e-2)
+})
